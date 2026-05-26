8f9ca05e43a951df418bba0cd00a3db4b4558864
caiohamamura <caiohamamura@gmail.com>
2026-05-26 01:09:44 +0000

    deps: update package metadata for Keras 3 migration and testthat
    - Replace keras with keras3 in Imports
    - Add magrittr to Imports (explicit pipe import)
    - Add testthat to Suggests
    - Update roxygen2 metadata (8.0.0)
    - Regenerate all .Rd files with updated imports and examples
    - Update rTLsDeep-package.R documentation

--- R/rTLsDeep-package.R
+++ R/rTLsDeep-package.R
@@ -1,10 +1,9 @@
-#' @keywords internal
-"_PACKAGE"
-
 #' rTLsDeep: Set of tools for post-hurricane individual tree-level damage classification from terrestrial laser scanning and deep learning.
 #'
 #' The rTLSDeep package provides options for:
 #' i) rotating and deriving 2D images from TLS 3D point clouds
-#' ii) calibrating and validating convolutional neural network (CNN) architectures and 
+#' ii) calibrating and validating convolutional neural network (CNN) architectures and
 #' iii) predicting post-hurricane damage severity at the individual tree level
-NULL
+#'
+#' @keywords internal
+"_PACKAGE"
