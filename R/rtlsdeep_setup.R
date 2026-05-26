fa41119618406eb2ea7cf5db278ef77c291b741c
caiohamamura <caiohamamura@gmail.com>
2026-05-26 01:10:22 +0000

    rtlsdeep_setup: add python_version parameter for Python installation
    Add optional python_version argument (default: 3.12) to allow users
    to specify which Python version to install when none is available.
    Also use initialize=TRUE in py_available() check for more reliable detection.

--- R/rtlsdeep_setup.R
+++ R/rtlsdeep_setup.R
@@ -1,12 +1,14 @@
 #' Setup python and tensorflow environment
 #'
+#' @param python_version Character string with Python version to install if not found. Default: "3.12".
+#'
 #' @return Nothing
 #' @export
-rtlsdeep_setup = function()
+rtlsdeep_setup = function(python_version = "3.12")
 {
-  if (reticulate::py_available() == FALSE)
+  if (reticulate::py_available(initialize = TRUE) == FALSE)
   {
-    reticulate::install_python()
+    reticulate::install_python(version = python_version)
   }
   if (reticulate::py_module_available('tensorflow') == FALSE)
   {
